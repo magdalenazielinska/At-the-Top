dba15b084941611350a4950c4915dffa3eb2c658
Magdalena Zielińska <magdalena.zielinska@hotmail.com>
2019-08-05 18:56:14 +0200

    Add Summit - html

--- AtTheTop-SPA/src/app/users/user-addsummits/user-addsummits.component.ts
+++ AtTheTop-SPA/src/app/users/user-addsummits/user-addsummits.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { Summit } from 'src/app/_models/summit';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-user-addsummits',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-addsummits.component.scss']
 })
 export class UserAddsummitsComponent implements OnInit {
+  user: User;
+  summit: Summit;
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  addSummit() {
+    console.log('szczyt dodany!');
   }
 
 }
